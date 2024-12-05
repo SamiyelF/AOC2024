7bde4b59b1333c796e94ff9bfaa566db656623a6
SamiyelF <samiyelfrazier@gmail.com>
2024-12-04 18:57:00 -0800

    d3p2

--- three/src/main.rs
+++ three/src/main.rs
@@ -1,51 +1,41 @@
 use regex::Regex;
-use reqwest;
-fn get_input(url: &str, session: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let client = reqwest::blocking::Client::new();
-    let response = client
-        .get(url)
-        .header("Cookie", format!("session={}", session))
-        .send()?
-        .text()?;
-    Ok(response)
-}
-fn valid_char(c: char) -> bool {
-    match c {
-        '0'..='9' => true,
-        'm' | 'u' | 'l' => true,
-        '(' | ')' | ',' => true,
-        _ => false,
-    }
-}
+use std::fs;
 fn main() {
-    let url = "https://adventofcode.com/2024/day/3/input";
-    let session = "";
-
-    let input: String = get_input(url, session).expect("No data found");
-    let mut cleaned: String = input.clone();
-    let mut removed = 0;
-    for (i, c) in input.chars().enumerate() {
-        if !valid_char(c) {
-            cleaned.remove(i - removed);
-            removed += 1;
-        }
-    }
-    let getmuls = Regex::new(r"(?m)(mul\()[0-9]{1,3},[0-9]{1,3}\)").unwrap();
+    let input = fs::read_to_string("input").expect("Failed to read input file");
+    let getmuls = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)").unwrap();
     let funcs: Vec<&str> = getmuls
-        .captures_iter(&cleaned)
+        .captures_iter(&input)
         .map(|captures| captures.get(0).unwrap().as_str())
         .collect();
-    let funcs_str = funcs.join(" ");
     let getvals = Regex::new(r"[0-9]{1,3}").unwrap();
-    let vals: Vec<i128> = getvals
-        .captures_iter(&funcs_str)
-        .map(|captures| captures.get(0).unwrap().as_str().parse::<i128>().unwrap())
-        .collect();
+    let mut vals: Vec<(i128, bool)> = Vec::new();
+    let mut go = true;
+    for func in funcs {
+        if func == "do()" {
+            go = true;
+        } else if func == "don't()" {
+            go = false;
+        } else {
+            let numbers: Vec<i128> = getvals
+                .find_iter(func)
+                .map(|m| m.as_str().parse::<i128>().unwrap())
+                .collect();
+            for num in numbers {
+                vals.push((num, go));
+            }
+        }
+    }
 
-    let groups: Vec<(i128, i128)> = vals.chunks(2).map(|chunk| (chunk[0], chunk[1])).collect();
+    let groups: Vec<((i128, bool), (i128, bool))> =
+        vals.chunks(2).map(|chunk| (chunk[0], chunk[1])).collect();
     let mut mults: Vec<i128> = Vec::new();
+    let mut domults: Vec<i128> = Vec::new();
     for pair in groups {
-        mults.push(pair.0 * pair.1);
+        if pair.0 .1 {
+            domults.push(pair.0 .0 * pair.1 .0);
+        }
+        mults.push(pair.0 .0 * pair.1 .0);
     }
     println!("{:?}", mults.iter().sum::<i128>());
+    println!("{:?}", domults.iter().sum::<i128>());
 }
